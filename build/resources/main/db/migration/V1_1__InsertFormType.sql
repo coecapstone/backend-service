4b8213758ac21340b2a24071fbe103aaf6340bc9
Xiaotong Yang <syangx38@gmail.com>
2021-03-12 12:32:48 -0800

    gitignore

--- build/resources/main/db/migration/V1_1__InsertFormType.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO form (`key`, `text`, `value`) VALUES
-('rei', 'Reimbursement', 'rei'),
-('trarei', 'Traval Reimbursement', 'trarei'),
-('pur', 'Purchase Request', 'pur'),
-('in', 'Pay an Invoice', 'in'),
-('pro', 'Procard Receipt', 'pro'),
-('tra', 'Travel Request', 'tra');
--- build/resources/main/sql/insertFormData.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO travel_request_form (legal_firstname, legal_lastname, is_approved)
-VALUES (:legal_firstname, :legal_lastname, 0);
-
